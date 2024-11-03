00c1c907cb192446b3aebfdcca37a55017c1396f
ChauQuang2003 <chauquang132003@gmail.com>
2024-11-04 04:04:41 +0700

    Update Bài Tập Ss2

--- Ss1.sql
+++ Ss1.sql
@@ -1,17 +1,20 @@
 create database Ss1;
 use Ss1;
  
-create table class (
-id int primary key,
-name varchar(50)
+CREATE TABLE class (
+    id INT PRIMARY KEY,
+    name VARCHAR(50)
 );
 
-create table teacher (
-id int primary key,
-name varchar(50),
-age int,
-country varchar(50)
+CREATE TABLE teacher (
+    id INT PRIMARY KEY,
+    name VARCHAR(50),
+    age INT,
+    country VARCHAR(50)
 );
 
-select *
-from Class, Teacher
+SELECT 
+    *
+FROM
+    Class,
+    Teacher
